92dde804cb1fe7cfe5f213302386c45677a8942b
NiklasVousten <24965952+NiklasVousten@users.noreply.github.com>
2024-09-08 16:31:05 +0200

    Added inital crab-hole service

---
+++ modules/services/crab-hole/default.nix
@@ -0,0 +1,103 @@
+{ config, lib, pkgs, ... }:
+with lib;
+let
+  cfg = config.services.crab-hole;
+  crab-hole = pkgs.callPackage ../../pkgs/crab-hole { };
+in
+{
+  options = {
+    services.crab-hole = { 
+      enable = mkEnableOption "Crab-hole Service";
+
+      user = mkOption {
+        type = types.str;
+        default = "crab-hole";
+        description = "User account under which crab-hole runs.";
+      };
+
+      group = mkOption {
+        type = types.str;
+        default = "crab-hole";
+        description = "Group account under which crab-hole runs.";
+      };
+
+      workDir = mkOption {
+        type = types.str;
+        default = "/var/lib/crab-hole";
+        description = "Crab-holes data directory.";
+      };
+
+      configFile = mkOption {
+        type = types.path;
+        description = "The config file of crab-hole";
+      };
+
+      blockListFiles = mkOption {
+        type = types.listOf types.path;
+        default = [];
+        description = "Path of the blocklists";
+      };
+
+      allowListFiles = mkOption {
+        type = types.listOf types.path;
+        default = [];
+        description = "Path of the allowlists";
+      };
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    systemd.services.crab-hole = {
+      wantedBy = [ "multi-user.target" ]; 
+      after = [ "network-online.target" ];
+      wants = [ "network-online.target" ];
+      description = "Start the crab-hole dns server";
+      environment = {
+        CRAB_HOLE_DIR = cfg.workDir;
+      };
+      serviceConfig = {
+        Type = "simple";
+        User = cfg.user;
+        Group = cfg.group;
+        WorkingDirectory = cfg.workDir;
+
+        ExecStart = ''${crab-hole}/bin/crab-hole'';
+
+        AmbientCapabilities = "CAP_NET_BIND_SERVICE";
+        CapabilityBoundingSet = "CAP_NET_BIND_SERVICE";
+
+        Restart = "on-failure";
+        RestartSec = 1;
+      };
+      
+      preStart = ''
+        cp -f '${cfg.configFile}' '${cfg.workDir}/config.toml'
+        ${builtins.concatStringsSep "\n" (map (file: "cp -f '${file}' '${cfg.workDir}/${lib.lists.last (builtins.split "/" (builtins.toString file))}'") cfg.blockListFiles)}
+        ${builtins.concatStringsSep "\n" (map (file: "cp -f '${file}' '${cfg.workDir}/${lib.lists.last (builtins.split "/" (builtins.toString file))}'") cfg.allowListFiles)}
+      '';
+    };
+
+    systemd.tmpfiles.rules = [
+      "d '${cfg.workDir}' 0750 ${cfg.user} ${cfg.group} - -"
+    ];
+
+    # Adding crab-hole user and group
+    users.users = optionalAttrs (cfg.user == "crab-hole") {
+      crab-hole = {
+        description = "Crab-hole service";
+        home = cfg.workDir;
+        group = cfg.group;
+        isSystemUser = true;
+      };
+    };
+
+    users.groups = optionalAttrs (cfg.group == "crab-hole") {
+      crab-hole = {};
+    };
+ 
+    environment.systemPackages = [
+      crab-hole
+    ];
+  };
+}
+
